5da0eace2ff85e35918401fd400aab213d0d355f
amitksms97 <amitksms97@gmail.com>
2021-05-01 12:24:25 +0530

    Add project files.

---
+++ LearningHashSet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myHash = new HashSet<String>();
+
+            myHash.Add("Hello");
+            myHash.Add("Hello");
+
+            String[] s = new String[] { "Hello" };  
+
+            Console.WriteLine(myHash.Count);
+
+            Console.WriteLine(myHash.Overlaps(s));
+
+        }
+    }
+}
